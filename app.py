f4936f5f2dab832e316d6a52037b50b8951b2e0d
sam-watts <sam.watts445@gmail.com>
2023-03-08 14:52:36 +0100

    add data for country averages, methodology, flight formatting

--- app.py
+++ app.py
@@ -6,11 +6,15 @@ import json
 from tqdm import tqdm
 from copy import deepcopy
 import plotly.express as px
+import plotly.graph_objects as go
 import reverse_geocoder as rg
 from unicodedata import lookup
 from google.oauth2 import service_account
 from google.cloud import storage
 import io
+from datetime import date, datetime
+from itertools import product
+import inspect
 
 
 @st.cache_data
@@ -40,7 +44,7 @@ def get_travel_annotation(from_cc, to_cc):
     return get_flag_emoji(from_cc) + " ➡ " + get_flag_emoji(to_cc)
 
 
-def extract_from_takeout(files: list[io.BytesIO]):
+def extract_from_takeout(files: list[io.BytesIO], start_date: date, end_date: date):
     data = []
     for file in files:
         with zipfile.ZipFile(file) as myzip:
@@ -51,7 +55,10 @@ def extract_from_takeout(files: list[io.BytesIO]):
                     "Takeout/Location History/Semantic Location History"
                 ):
                     year = int(name.split("/")[-2])
-                    if year >= 2020:
+                    month_name = name.split("/")[-1].split("_")[-1].split(".")[0]
+                    month = datetime.strptime(month_name, "%B").month
+
+                    if start_date <= date(year, month, 1) <= end_date:
                         with myzip.open(name) as f:
                             data.append(json.load(f))
 
@@ -98,36 +105,27 @@ def parse_activities(data):
     return activities
 
 
-def calculate_g_co2_eq(activity_type, distance_meters):
-    match activity_type:
-        case "CYCLING":
-            # cycling average diet
-            factor = 18.4
-        case "RUNNING" | "WALKING":
-            # assumed running is the same as walking (average diet)
-            factor = 24.15
-        case "IN_TRAIN":
-            # mean of diesel and electric trains
-            factor = (23.46 + 48.64) / 2
-        case "IN_BUS":
-            factor = 41.63
-        case "FLYING":
-            # mean of short and long haul flights
-            factor = (239 + 340) / 2
-        case "IN_PASSENGER_VEHICLE":
-            # car (diesel) - no sharing!
-            factor = 182.94
-        case "IN_SUBWAY":
-            # train (electric)
-            factor = 23.46
-        case "IN_TRAM":
-            # train (electric)
-            factor = 23.46
-        case "UNKNOWN_ACTIVITY_TYPE":
-            # TODO this is made up right now!
-            factor = 100
-        case _:
-            factor = np.nan
+def calculate_g_co2_eq(activity_type: str, distance_meters: float):
+    # units: g CO2eq / person / km
+    factors = {
+        "CYCLING": 18.4,  # cycling average diet
+        "RUNNING": 24.15,  # assumed running is the same as walking
+        "WALKING": 24.15,
+        "IN_TRAIN": (23.46 + 48.64) / 2,  # mean of diesel and electric trains
+        "IN_BUS": 41.63,
+        "FLYING": (239 + 340) / 2,  # mean of short and long haul flights
+        "IN_PASSENGER_VEHICLE": 182.94,  # car (diesel) - no sharing!
+        "IN_SUBWAY": 23.46,  # train (electric)
+        "IN_TRAM": 23.46,  # train (electric)
+    }
+
+    factor = factors.get(
+        activity_type,
+        # fallback eg. for `UNKNOWN_ACTIVITY_TYPE``
+        # take an average of all the factors
+        # some other edge cases like `SKIING`` also fall into this
+        np.mean(list(factors.values())),
+    )
 
     return distance_meters / 1000 * factor
 
@@ -164,7 +162,7 @@ with st.sidebar:
             """
         )
 
-    st.markdown("Or:")
+    st.markdown("**Or:**")
     use_sample_data = st.checkbox("Use sample data?")
 
     if use_sample_data:
@@ -175,6 +173,13 @@ with st.sidebar:
         type="zip",
         disabled=uploader_disabled,
         accept_multiple_files=True,
+    )
+
+    start_date, end_date = st.slider(
+        "Select a date range:",
+        date(2017, 1, 1),
+        date.today(),
+        (date(2019, 1, 1), date.today()),
     )
 
 
@@ -203,8 +208,8 @@ if not files:
     st.image("timeline_screenshot.png")
 else:
     reduction_levels = st.checkbox(
-        "Show emissions reduction levels?",
-        value=True,
+        "Show 1.5°C emissions reduction targets?",
+        value=False,
         help=(
             "These dotted red lines show the required CO2eq emissions / person / year"
             " up to 2050, to have a 50% chance of limiting global warming to 1.5°C. See"
@@ -212,7 +217,7 @@ else:
             " logic this is based on."
         ),
     )
-    raw_data = extract_from_takeout(files)
+    raw_data = extract_from_takeout(files, start_date, end_date)
 
     parsed_data = parse_activities(raw_data)
     df = pd.DataFrame(parsed_data)
@@ -263,6 +268,8 @@ else:
         lambda x: get_travel_annotation(x["start_country"], x["end_country"]), axis=1
     )
 
+    df = df.sort_values("start_timestamp")
+
     aggregators = dict(
         total_distance_km=pd.NamedAgg("distance_meters", lambda x: x.sum() / 1000),
         tonnes_co2_eq=pd.NamedAgg("tonnes_co2_eq", "sum"),
@@ -285,12 +292,83 @@ else:
     )
 
     with tabs[0]:
+
+        def clean_columns(x):
+            return (
+                x.str.lower()
+                .str.replace(" ", "_")
+                .str.replace("(", "")
+                .str.replace(")", "")
+                .str.replace("-", "_")
+                .str.replace("_+", "_", regex=True)
+            )
+
+        emissions_by_country = pd.read_csv("data/per-capita-co2-transport.csv")
+
+        emissions_by_country.columns = clean_columns(emissions_by_country.columns)
+        emissions_by_country = emissions_by_country.rename(
+            columns={"transport_per_capita": "tonnes_co2_eq"}
+        )
+        del emissions_by_country["code"]
+
+        fill_years = range(2020, date.today().year + 1)
+        extra_rows = pd.DataFrame(
+            product(emissions_by_country["entity"].unique(), fill_years),
+            columns=["entity", "year"],
+        )
+        extra_rows["tonnes_co2_eq"] = np.nan
+
+        emissions_by_country = pd.concat([emissions_by_country, extra_rows], axis=0)
+        emissions_by_country = emissions_by_country.sort_values(["entity", "year"])
+        emissions_by_country = emissions_by_country.fillna(method="ffill")
+
+        emissions_by_country_international_flights = pd.read_csv(
+            "data/per-capita-co2-international-flights-adjusted.csv"
+        )
+        emissions_by_country_international_flights.columns = clean_columns(
+            emissions_by_country_international_flights.columns
+        )
+
+        emissions_by_country = pd.merge(
+            emissions_by_country,
+            emissions_by_country_international_flights[
+                ["entity", "per_capita_international_co2_adjusted"]
+            ],
+            on="entity",
+        )
+
+        # add the international flights to the transport emissions,
+        # dividing by 1000 to convert to tonnes
+        emissions_by_country["tonnes_co2_eq"] += (
+            emissions_by_country["per_capita_international_co2_adjusted"] / 1000
+        )
+
+        # add in estimated emissions from shipping, a flat 11.6% of total
+        # emissions globally
+        shipping_estimate = 0.116 * emissions_by_country["tonnes_co2_eq"] / (1 - 0.116)
+        emissions_by_country["tonnes_co2_eq"] += shipping_estimate
+
+        emissions_by_country = emissions_by_country[
+            emissions_by_country["year"].isin(agg["year"].unique())
+        ]
+        comp_countries = st.multiselect(
+            (
+                "Select countries to compare for an average persons yearly transport"
+                " CO2 emissions:"
+            ),
+            emissions_by_country["entity"].unique(),
+        )
+
         fig = px.bar(
             agg,
             x="year",
             y="tonnes_co2_eq",
             color="activity_type",
-            hover_data=["percentage_yearly_emissions"],
+            hover_data=[
+                "percentage_yearly_emissions",
+                "number_of_activities",
+                "total_distance_km",
+            ],
         )
         fig.update_layout(yaxis_title="Tonnes CO2eq")
         fig.update_xaxes(type="category")
@@ -298,20 +376,92 @@ else:
             fig.add_hline(
                 y=3.5,
                 line_dash="dash",
-                annotation_text="3.5 tons CO2eq / person / year by 2030",
+                annotation_text="3.5 tonnes CO2eq / person / year by 2030",
                 line_color="red",
             )
             fig.add_hline(
                 y=1.64,
                 line_dash="dash",
-                annotation_text="1.64 tons CO2eq / person / year by 2040",
+                annotation_text="1.64 tonnes CO2eq / person / year by 2040",
                 line_color="red",
             )
+
+        for country in comp_countries:
+            fig.add_trace(
+                go.Scatter(
+                    x=emissions_by_country[emissions_by_country["entity"] == country][
+                        "year"
+                    ],
+                    y=emissions_by_country[emissions_by_country["entity"] == country][
+                        "tonnes_co2_eq"
+                    ],
+                    mode="lines",
+                    name=f"{country} per capita transport emissions",
+                    line=dict(dash="dash"),
+                )
+            )
+
         st.plotly_chart(fig)
+
+        with st.expander(":scientist: Sources"):
+            st.markdown(
+                """
+                ### Transport Emissions Factors
+
+                These are taken from Oliver Corradi's great general
+                [post](https://oliviercorradi.com/climate-change/#transportation--reduce-long-distance-travels)
+                on climate change. The code to calculate the emissions is show below:
+                """
+            )
+            st.markdown(f"```python \n{inspect.getsource(calculate_g_co2_eq)}```")
+            st.markdown(
+                """
+                ### Per Capita Transport Emissions by Country
+
+                Several sources from Our World in Data Were used, as there is no
+                single source I could find for this data across all transport
+                sectors at the time of writing (please reach out if I'm wrong
+                here!)
+
+                Drawbacks:
+                * Doesn't account for non-CO2 greenhouse gas emissions
+                * Doesn't account shipping emissions directly - an global
+                adjustment factor is made
+                * Data age - see indiviual points below
+
+                [per-capita-co2-transport.csv - up to 2019](
+                    https://ourworldindata.org/grapher/per-capita-co2-transport):
+
+                "... the average per capita emissions of carbon dioxide from
+                transport each year. This includes road, train, bus and
+                domestic air travel but does not include international aviation
+                and shipping."
+
+                Data from 2019 is used to fill in missing data for 2020 onwards.
+
+                [per-capita-co2-international-flights-adjusted.csv - 2018 only](
+                    https://ourworldindata.org/grapher/per-capita-co2-international-flights-adjusted):
+
+                "International aviation emissions are allocated to the country of
+                departure, then adjusted for tourism by multiplying this figure
+                by the ratio of inbound-to-outbound travelers. This attempts to
+                distinguish between locals traveling abroad and foreign visitors
+                traveling to that country on the same flight."
+
+                As the data only covers one year, it is used to fill in all other years.
+
+                [Global transport totals](https://ourworldindata.org/transport)
+
+                This data is used to correct for the fact that the above data
+                does not account for shipping. A global adjustment factor of
+                10.6% is used.
+
+                """
+            )
 
     with tabs[1]:
         fig = px.bar(
-            df[df["activity_type"] == "FLYING"].sort_values("year"),
+            df[df["activity_type"] == "FLYING"],
             x="year",
             y="tonnes_co2_eq",
             hover_data=[
@@ -321,23 +471,60 @@ else:
                 "end_country",
                 "start_date",
             ],
+            barmode="stack",
             text="travel_annotation",
         )
+        fig.update_layout(uniformtext_minsize=8, uniformtext_mode="hide")
 
         fig.update_xaxes(type="category")
         fig.update_layout(yaxis_title="Tonnes CO2eq")
+
+        fig.update_layout(margin=dict(r=100))
+        fig.add_annotation(
+            xref="x domain",
+            yref="y domain",
+            x=1.05,
+            y=0.2,
+            showarrow=False,
+            text="January",
+            xanchor="left",
+        )
+        fig.add_annotation(
+            xref="x domain",
+            yref="y domain",
+            x=1.05,
+            y=0.7,
+            showarrow=False,
+            text="December",
+            xanchor="left",
+        )
+        # add an arrow pointing from the january annotation to the december
+        # annotation
+        fig.add_annotation(
+            xref="x domain",
+            yref="y domain",
+            ayref="y domain",
+            ay=0.15,
+            y=0.75,
+            x=1.03,
+            ax=1.03,
+            showarrow=True,
+            arrowcolor="white",
+            arrowhead=4,
+            align="center",
+        )
 
         if reduction_levels:
             fig.add_hline(
                 y=3.5,
                 line_dash="dash",
-                annotation_text="3.5 tons CO2eq / person / year by 2030",
+                annotation_text="3.5 tonnes CO2eq / person / year by 2030",
                 line_color="red",
             )
             fig.add_hline(
                 y=1.64,
                 line_dash="dash",
-                annotation_text="1.64 tons CO2eq / person / year by 2040",
+                annotation_text="1.64 tonens CO2eq / person / year by 2040",
                 line_color="red",
             )
 
